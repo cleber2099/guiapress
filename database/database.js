1a23530f210e1b78db5ba723087863978c44cb69
cleber2099 <primermito@gmail.com>
2022-03-26 05:24:41 -0300

    base inicial do projeto

---
+++ database/database.js
@@ -0,0 +1,7 @@
+const Sequelize = require("sequelize");
+const connection = new Sequelize('guiapress','root','2099',{
+    host: 'localhost',
+    dialect: 'mysql'
+}); 
+
+module.exports= connection;
